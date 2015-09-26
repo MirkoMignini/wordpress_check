8be23c41f9cd5c0c91b43d289bf1b5e5725f4fd3
MirkoMignini <mirko.mignini@gmail.com>
2015-09-26 12:16:37 +0200

    first commit

---
+++ lib/wordpress_check.rb
@@ -0,0 +1,2 @@
+module WordpressCheck
+end
---
+++ test/wordpress_check_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WordpressCheckTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, WordpressCheck
+  end
+end
