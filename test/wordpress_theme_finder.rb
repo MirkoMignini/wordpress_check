5fba4bb5661848469c08bce52e64efd7265b83d8
MirkoMignini <mirko.mignini@gmail.com>
2015-09-29 17:18:30 +0200

    added tests for non wordpress sites and params

--- test/wordpress_theme_finder.rb
+++ test/wordpress_theme_finder.rb
@@ -2,12 +2,15 @@ require 'minitest/autorun'
 require_relative '../lib/wordpress_theme_finder'
 
 class TestAll < Minitest::Test
-  def test_module
+  def test_not_wordpress_site
+    result = WordpressThemeFinder.check('http://www.google.com')
+    assert_nil result
+  end
 
-    WordpressThemeFinder
-
+  def test_wordpress_site
     result = WordpressThemeFinder.check('https://wp-themes.com/twentyfifteen/')
-    p result
-    #assert_equal true, false
+    assert_kind_of Hash, result
+
+    assert_equal 'Twenty Fifteen', result['Theme Name']
   end
 end
